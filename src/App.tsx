4f47416eb47b94311abaf33cc9206f4b2b98a4cf
Juniper Alanna Berry <101523445+juniperab@users.noreply.github.com>
2022-11-26 11:01:12 -0500

    Set up basic layout for App view

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import styled from "styled-components";
+
+const Header = styled.h1`
+  text-align: center;
+`
+
+const Main = styled.div`
+  display: grid;
+  grid: 1fr / repeat(3, 1fr);
+  column-gap: 10px;
+  width: 800px;
+  margin: auto;
+`
+
+const Section = styled.div`
+  border: 2px solid black;
+  display: inline-block;
+  left: 0;
+  right: 0;
+  padding: 0 10px;
+  text-align: center;
+`
+
+const SectionHeader = styled.h4`
+    margin: 10px 0;
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+      <>
+          <Header>Prismatic</Header>
+          <Main>
+              <Section>
+                  <SectionHeader>Colour Picker</SectionHeader>
+                  <p>Hello world</p>
+              </Section>
+              <Section>
+                  <SectionHeader>Response View</SectionHeader>
+                  <p>Hello world</p>
+              </Section>
+              <Section>
+                  <SectionHeader>Debug Info</SectionHeader>
+                  <p>Hello world</p>
+              </Section>
+          </Main>
+      </>
   );
 }
 
