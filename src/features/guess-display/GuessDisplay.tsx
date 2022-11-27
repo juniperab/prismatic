c74c685ccdfd53c37687b07821fdb1207d8dc2ad
Juniper Alanna Berry <101523445+juniperab@users.noreply.github.com>
2022-11-26 21:38:53 -0500

    Add initial layout for guess responses

--- src/features/guess-display/GuessDisplay.tsx
+++ src/features/guess-display/GuessDisplay.tsx
@@ -1,7 +1,38 @@
+import styled from "styled-components";
+
+const GuessList = styled.div`
+  height: 360px;
+  overflow-y: scroll;
+`
+
+const Guess = styled.div`
+  height: 25px;
+  width: auto;
+  margin: 10px;
+  padding: 5px;
+  border: 1px solid black;
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: stretch;
+  align-items: center;
+  gap: 10px;
+  div {
+    display: inline-block;
+    height: 100%;
+    background-color: blue;
+    flex: none;
+    flex-grow: 1;
+  }
+`
+
 export function GuessDisplay() {
     return (
-        <>
-            <p>Guesses go here</p>
-        </>
+        <GuessList>
+            <Guess><div style={{flexGrow: 2}}/><div/><div/><div/></Guess>
+            <Guess/>
+            <Guess/>
+            <Guess/>
+            <Guess/>
+        </GuessList>
     )
 }
