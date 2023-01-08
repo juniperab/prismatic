9fe617347794b56759c5ec1d9e43a34f30cd55da
Juniper Alanna Berry <101523445+juniperab@users.noreply.github.com>
2023-01-08 12:12:33 -0500

    Fix additional polyfill problems

--- config-overrides.js
+++ config-overrides.js
@@ -1,23 +1,24 @@
-const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
 const webpack = require('webpack');
 
 module.exports = (config) => {
   const fallback = config.resolve.fallback || {};
   Object.assign(fallback, {
-    "crypto": require.resolve("crypto-browserify"),
-    "stream": require.resolve("stream-browserify"),
-    "assert": require.resolve("assert"),
-    "http": require.resolve("stream-http"),
-    "https": require.resolve("https-browserify"),
-    "os": require.resolve("os-browserify"),
-    "url": require.resolve("url"),
+    'assert': require.resolve('assert'),
+    'console': require.resolve('console-browserify'),
+    'crypto': require.resolve('crypto-browserify'),
     'fs': require.resolve('browserify-fs'),
+    'http': require.resolve('stream-http'),
+    'https': require.resolve('https-browserify'),
     'net': require.resolve('net-browserify'),
-    'tls': require.resolve('tls-browserify')
+    'os': require.resolve('os-browserify'),
+    'path': require.resolve('path-browserify'),
+    'stream': require.resolve('stream-browserify'),
+    'tls': require.resolve('tls-browserify'),
+    'url': require.resolve('url'),
+    'zlib': require.resolve('browserify-zlib')
   })
   config.resolve.fallback = fallback;
   config.plugins = (config.plugins || []).concat([
-    new NodePolyfillPlugin(),
     new webpack.ProvidePlugin({
       process: 'process/browser',
       Buffer: ['buffer', 'Buffer']
